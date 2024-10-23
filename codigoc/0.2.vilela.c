e9c3eab18b4e23e396d45523e9d59c9b26823401
ThiagoCHAPECO <guampas@uol.com.br>
2024-10-23 17:42:04 -0300

    vilela

--- codigoc/0.2.vilela.c
+++ codigoc/0.2.vilela.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 int main(){
 	int v1;
-	float end, end2;
+	float end1, end2;
 	char leia;
 	
 	printf("Digite o valor do produto: ");
 	scanf("%d", &v1);
 	
-	end = v1 - (v1 * 0.2);
-	printf("Esse eh o valor do primeiro desconto aplicado: %.2f\n", end);
+	end1 = v1 - (v1 * 0.2);
+	printf("Esse eh o valor do primeiro desconto aplicado: %.2f\n", end1);
 
 	printf("Vai ter algum desconto a mais?: ");
 	scanf(" %c", &leia);
 	
 	if(leia == 's'||leia == 'S'){	
-		end2 = end - (end * 0.1);
+		end2 = end1 - (end1 * 0.1);
 	}
 	
 	printf("Esse foi o desconto final: %.2f\n", end2);
